a06a2643fa1b683cf44faeff3783747b4144179c
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2019-09-12 18:18:26 +0600

    HM7

---
+++ src/com/company/Magic.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Magic extends Player implements SuperAdults{
+    private int defence ;
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    @Override
+    public void superAdults() {
+
+    }
+}
---
+++ src/com/company/Mental.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Mental extends Player implements SuperAdults {
+    private int superAdults ;
+
+    public int getSuperAdults() {
+        return superAdults;
+    }
+
+    public void setSuperAdults(int superAdults) {
+        this.superAdults = superAdults;
+    }
+
+    @Override
+    public void superAdults() {
+
+    }
+}
